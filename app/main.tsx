1a182c242a50b158982a391df62fabc9b5cf0d43
Mo Wang <mo.hy.wang@gmail.com>
2016-05-01 16:51:33 -0700

    :fdS:

--- app/main.tsx
+++
@@ -1,7 +0,0 @@
-/// <reference path="../typings/main.d.ts" />
-
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './views/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
